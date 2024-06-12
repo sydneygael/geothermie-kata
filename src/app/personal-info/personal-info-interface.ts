d212f097a381267ca3328487157cf042c5dfa1f4
sydney.adjou-moumouni <sydgael@gmail.com>
2024-06-12 14:51:31 +0200

    feat(personalinfo) : add form

---
+++ src/app/personal-info/personal-info-interface.ts
@@ -0,0 +1,8 @@
+export interface PersonalInfo {
+    civilite: string;
+    nom: string;
+    prenom: string;
+    email: string;
+    telephone: string;
+  }
+  
--- src/app/personal-info/personal-info.component.ts
+++ src/app/personal-info/personal-info.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PersonalInfo } from './personal-info-interface';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-personal-info',
   standalone: true,
-  imports: [],
   templateUrl: './personal-info.component.html',
-  styleUrl: './personal-info.component.css'
+  imports: [FormsModule,CommonModule],
+  styleUrls: ['./personal-info.component.css']
 })
 export class PersonalInfoComponent {
 
+  personalInfo: PersonalInfo = {
+    civilite: '',
+    nom: '',
+    prenom: '',
+    email: '',
+    telephone: ''
+  };
+
+  constructor( private router: Router) {}
+
+  onSubmit() {
+  }
 }
