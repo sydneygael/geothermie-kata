6914d2310de1786d1b6e31ea8ee9b8c2cf3c8b5c
sydney.adjou-moumouni <sydgael@gmail.com>
2024-06-12 13:33:25 +0200

    chore : generation du personnal info component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/personal-info', pathMatch: 'full' },
+    { path: 'personal-info', component: PersonalInfoComponent }
+];
